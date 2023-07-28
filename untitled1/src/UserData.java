6836198eee8fec7dad83705af78f849070ec99a6
tranquilo108 <118503079+tranquilo108@users.noreply.github.com>
2023-07-29 02:02:30 +0700

    Task6

---
+++ untitled1/src/UserData.java
@@ -0,0 +1,23 @@
+public class UserData {
+    private final String login;
+    private final String password;
+    private final String confirm;
+
+    public UserData(String login, String password, String confirm) {
+        this.login = login;
+        this.password = password;
+        this.confirm = confirm;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getConfirm() {
+        return confirm;
+    }
+}
